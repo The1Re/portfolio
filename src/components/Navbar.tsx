9136b424c9eee30aedb50d360e1a901a2e319123
The1Re <kanesh132@gmail.com>
2024-12-09 20:23:59 +0700

    edit resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,63 +31,35 @@ function Navbar() {
   }, [])
 
   return (
-//     <nav className="w-full flex flex-col md:flex-row items-start justify-between bg-navbar p-4 lg:px-48 py-8 fixed top-0 z-10">
-//     <div className=" w-full flex justify-between items-center">
-//         <span className="text-3xl font-bold text-primary">Port<span className="text-white">folio</span></span>
-//         <button 
-//             onClick={() => setIsOpen(!isOpen)} 
-//             type="button" 
-//             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-content" 
-//             aria-expanded={isOpen}
-//         >
-//             <i className={`fa-solid ${isOpen ? 'fa-times' : 'fa-bars'} text-xl`}></i>
-//         </button> 
-//     </div>
-//     <div className={`nav-right w-full flex-col items-start space-y-4 md:space-x-10 md:space-y-0 md:flex-row md:flex md:items-end ${isOpen ? 'flex mt-4 md:mt-0' : 'hidden'}`}>
-//         {sections.map((section, index) => (
-//             <a 
-//                 key={index} 
-//                 href={`#${section}`} 
-//                 className={`${activeSection === section ? 'bg-primary px-4 py-1 rounded-lg transition-colors font-bold' : ''} ` }
-//             >
-//                 {section}
-//             </a>
-//         ))}
-//     </div>
-// </nav>
-
-<nav className="w-full flex flex-col md:flex-row justify-between bg-navbar p-4 lg:px-48 py-4 md:py-8 fixed top-0 z-10">
-        <div className='w-full flex justify-between items-center'>
-          <div className="nav-left">
-              <span className="text-3xl font-bold text-primary">Port<span className="text-white">folio</span></span>
-          </div>
-          <button 
-            onClick={() => setIsOpen(!isOpen)} 
-            type="button" 
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-content" 
-            aria-expanded={isOpen}
-          >
-            <i className={`fa-solid ${isOpen ? 'fa-times' : 'fa-bars'} text-xl`}></i>
-          </button> 
-
+    <nav className="w-full flex flex-col md:flex-row justify-between bg-navbar p-4 lg:px-48 py-4 md:py-8 fixed top-0 z-10">
+      <div className='w-full flex justify-between items-center'>
+        <div className="nav-left">
+            <span className="text-3xl font-bold text-primary">Port<span className="text-white">folio</span></span>
         </div>
-        <div className={`nav-right flex-col items-start space-y-4 md:space-x-10 md:space-y-0 md:flex-row md:flex md:items-center ${isOpen ? 'flex mt-4 md:mt-0' : 'hidden'}`}>
-          {
-            sections.map((section, index) => (
-              <a 
-                key={index} 
-                href={`#${section}`} 
-                className={`${activeSection === section ? 'bg-primary px-4 py-1 rounded-lg transition-colors font-bold' : ''} ` }
-              >
-                {section}
-              </a>
-            ))
-          }
-        </div>
+        <button 
+          onClick={() => setIsOpen(!isOpen)} 
+          type="button" 
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-content" 
+          aria-expanded={isOpen}
+        >
+          <i className={`fa-solid ${isOpen ? 'fa-times' : 'fa-bars'} text-xl`}></i>
+        </button> 
+
+      </div>
+      <div className={`nav-right flex-col items-start space-y-4 md:space-x-10 md:space-y-0 md:flex-row md:flex md:items-center ${isOpen ? 'flex mt-4 md:mt-0' : 'hidden'}`}>
+        {
+          sections.map((section, index) => (
+            <a 
+              key={index} 
+              href={`#${section}`} 
+              className={`${activeSection === section ? 'bg-primary px-4 py-1 rounded-lg transition-colors font-bold' : ''} ` }
+            >
+              {section}
+            </a>
+          ))
+        }
+      </div>
     </nav> 
-
-
-
   )
 }
 
